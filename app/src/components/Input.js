f4de5abdd66d9c7bc3989b1602d028e8812fbe05
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-03-01 16:52:47 +0000

    component files

---
+++ app/src/components/Input.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Input() {
+    return (
+        <input className="list-input" placeholder="What would you like to add?">To-Do List</input>
+    )
+}
+
+export default Input; 
