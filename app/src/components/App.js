698d1fc17be46785afadfc4814873480e1a35b5a
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-03-02 18:15:42 +0000

    typed some pseudocode

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -4,20 +4,43 @@ import Todo from "./Todo";
 //import Input from "./Input";
 
 function App() {
-    const [todo, setTodo] = useState([])
-    const [count, setCount] = useState(todo.length);
+    const [todos, setTodos] = useState([])
+    const [count, setCount] = useState(todos.length);
     const [input, setInput] = useState('');
+    const [active, setActive] = useState(true);
+    let displayList = handleDisplay();
 
     function inputChange(event) {
         setInput(event.target.value);
     }
 
     function handleClick(event) {
-        let newItem = [...todo, input]
-        setTodo(newItem);
+        let newItem = [...todos, input]
+        setTodos(newItem);
         setCount(newItem.length);
     }
 
+    function handleCheck() {
+        console.log('check');
+        setActive(false);
+    }
+
+    function handleDisplay(item) {
+        let displayList = [];
+        displayList = todos.filter(todo => {
+            if (todo.active) {
+               return displayList.push(todo);
+            } else {
+                return displayList.push(todo);    
+            }        
+        })
+        return displayList;
+    }
+
+    function deleteList() {
+
+    }
+    console.log(displayList);
     return (
         <div className="container">
             <div className="row d-flex">
@@ -36,13 +59,29 @@ function App() {
                     <button onClick={handleClick}>Add</button>
                 </div>
             </div>
-            {todo.map(listIt => {
+            {todos.map(todo => {
                 return (
-                    <Todo key={listIt} listIt={listIt} />
+                    <div key={todo} className="row">
+                        <div className="col d-flex justify-content-center bg-light">
+                            <ul className="list-group">
+                                <li className="list-group-item">
+                                    <input type="checkbox" value={active} onClick={handleCheck} />
+                                    {todo}
+                                    {/* make a delete button */}
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 )
-              })
+            })
             }
-
+            <div className="row d-flex justify-content-center">
+                <div className="col d-flex justify-content-center bg-light">
+                    <button type="button" onClick={handleDisplay}>Active</button>
+                    <button type="button" onClick={handleDisplay}>Completed</button>
+                    <button type="button" onClick={deleteList}>Delete</button>
+                </div>
+            </div>
         </div>
     )
 }
